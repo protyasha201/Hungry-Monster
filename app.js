9410d8fa2ebc552da23c86564150b29dc066e464
protyasha201 <protyasha0999@gmail.com>
2021-02-07 23:02:08 +0600

    updated

--- app.js
+++ app.js
@@ -45,24 +45,50 @@ function display(id, images, foodName) {
             const ingredients = data.meals[0];
             const measures = data.meals[0];
             const details = `
-        <div class="detail-image">
-        <img src="${images}">
-        </div>
-        <h2>${foodName}</h2>
-        <h3>Ingredients</h3>
-        <ul class="ingredient-list">
-        <li>${measures.strMeasure1} ${ingredients.strIngredient1}</li>
-        <li>${measures.strMeasure2} ${ingredients.strIngredient2}</li>
-        <li>${measures.strMeasure3} ${ingredients.strIngredient3}</li>
-        <li>${measures.strMeasure4} ${ingredients.strIngredient4}</li>
-        <li>${measures.strMeasure5} ${ingredients.strIngredient5}</li>
-        <li>${measures.strMeasure6} ${ingredients.strIngredient6}</li>
-        <li>${measures.strMeasure7} ${ingredients.strIngredient7}</li>
-        <li>${measures.strMeasure8} ${ingredients.strIngredient8}</li>
-        <li>${measures.strMeasure9} ${ingredients.strIngredient9}</li>
-        <li>${measures.strMeasure10} ${ingredients.strIngredient10}</li>
-            </ul>
+                <div class="detail-image">
+                    <img src="${images}">
+                </div>
+                <h2>${foodName}</h2>
+                <h3>Ingredients</h3>
+                <ul class="ingredient-list">
+                    <li>${measures.strMeasure1} ${ingredients.strIngredient1}</li>
+                    <li>${measures.strMeasure2} ${ingredients.strIngredient2}</li>
+                    <li>${measures.strMeasure3} ${ingredients.strIngredient3}</li>
+                    <li>${measures.strMeasure4} ${ingredients.strIngredient4}</li>
+                    <li>${measures.strMeasure5} ${ingredients.strIngredient5}</li>
+                    <li>${measures.strMeasure6} ${ingredients.strIngredient6}</li>
+                    <li>${measures.strMeasure7} ${ingredients.strIngredient7}</li>
+                    <li>${measures.strMeasure8} ${ingredients.strIngredient8}</li>
+                    <li>${measures.strMeasure9} ${ingredients.strIngredient9}</li>
+                    <li>${measures.strMeasure10} ${ingredients.strIngredient10}</li>
+                </ul>
             `;
             foodDetails.innerHTML = details;
         })
+}
+
+ //showing alert
+    
+ const searchedMealIs = searchMeal.value;
+ function checkAvailability(nameIs){
+     fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+ .then(res => res.json())
+ .then(data => {
+     const categories = data.categories;
+     const capitalNameIs = nameIs.toUpperCase();
+     let isAvailable = false;
+     categories.forEach(item => {
+         const foodCategoryName = item.strCategory;
+         const capitalFoodCategory = foodCategoryName.toUpperCase();
+         if(capitalNameIs === capitalFoodCategory){
+             isAvailable = true;
+         }
+         else{
+             isAvailable = false;
+         }
+     });
+     if(isAvailable == false){
+         alert("Not Available");
+     }
+ })
 }
