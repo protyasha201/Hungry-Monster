909deab56b5409d2eb04ecceb410d2413b2dce92
protyasha201 <protyasha0999@gmail.com>
2021-02-07 20:49:02 +0600

    completed

--- app.js
+++ app.js
@@ -8,17 +8,18 @@ searchNow.addEventListener("click", () => {
             foodList.forEach(food => {
                 const foodNames = food.strMeal;
                 const foodImages = food.strMealThumb;
+                const foodId = food.idMeal;
                 const foodsContainer = document.getElementById("foodsContainer");
                 const searchedFor = document.getElementById("searchedFor");
                 const foodDiv = document.createElement("div");
                 foodDiv.className = "food-items";
 
                 const foodInfo = `
-                <div onclick="display('${foodNames}')">
-                <div class="image">
-                    <img src="${foodImages}">
-                </div>
-                <div>${foodNames}</div>
+                <div onclick="display(${foodId}, '${foodImages}', '${foodNames}')">
+                    <div class="image">
+                        <img src="${foodImages}">
+                    </div>
+                    <div>${foodNames}</div>
                 </div>
                 `;
 
@@ -28,6 +29,29 @@ searchNow.addEventListener("click", () => {
         })
 })
 
-function display(name){
-    console.log(name);
+function display(id, images, foodName) {
+    fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id + '')
+        .then(res => res.json())
+        .then(data => {
+            const foodDetails = document.getElementById("food-details");
+            foodDetails.style.display = "block";
+            const ingredients = data.meals[0];
+            const measures = data.meals[0];
+            const details = `
+            <div class="detail-image">
+                <img src="${images}">
+            </div>
+            <h2>${foodName}</h2>
+            <h3>Ingredients</h3>
+            <ul class="ingredient-list">
+                <li>${measures.strMeasure1} ${ingredients.strIngredient1}</li>
+                <li>${measures.strMeasure2} ${ingredients.strIngredient2}</li>
+                <li>${measures.strMeasure3} ${ingredients.strIngredient3}</li>
+                <li>${measures.strMeasure4} ${ingredients.strIngredient4}</li>
+                <li>${measures.strMeasure5} ${ingredients.strIngredient5}</li>
+                <li>${measures.strMeasure6} ${ingredients.strIngredient6}</li>
+            </ul>
+        `;
+            foodDetails.innerHTML = details;
+        })
 }
